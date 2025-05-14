7ccbfecd8f5a47be8656a159511247060c144a6a
Stupnikov Dmitry <d_lis82@mail.ru>
2025-05-14 18:58:54 +0300

    Функция поиска без таймера

--- search.py
+++ search.py
@@ -1,1 +1,11 @@
 #Линейный поиск и сложности алгоритмов
+
+def search(arr, n):
+    ind = 0
+    for i in arr:
+        if i == n:
+            return ind
+        ind += 1
+    return -1
+
+print(search([1, 2, 3, 50, 70], 70))
